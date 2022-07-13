ab06c4a08680c5febffc2f2f64a8724df7c7fc50
Yunhao Liu <yunhao981@hotmail.com>
2022-07-14 01:28:26 +0800

    added order page and controller

--- src/main/java/com/example/tacocloud/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -49,5 +50,13 @@ public class DesignTacoController {
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        // TODO: finish this in Ch.03, adding some DB processing logic
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 }
