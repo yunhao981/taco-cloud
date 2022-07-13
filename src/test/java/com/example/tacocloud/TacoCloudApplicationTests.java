0ba40aac609921204d5855617220a04e2e2154bb
Yunhao Liu <yunhao981@hotmail.com>
2022-07-14 00:31:05 +0800

    updated design controller

---
+++ src/test/java/com/example/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
