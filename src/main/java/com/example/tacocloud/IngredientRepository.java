4b8a56a31645dfcb75fe549796f7bcba9f0e3b1b
Yunhao Liu <yunhao981@hotmail.com>
2022-07-27 00:27:36 +0800

    added jdbc template examples

---
+++ src/main/java/com/example/tacocloud/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.example.tacocloud;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Ingredient findOne(String id);
+
+    Ingredient save(Ingredient ingredient);
+
+}
